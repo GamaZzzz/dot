066825fb7306bb91b132d17edfb23a72fe509999
gaoshuai <43263975+Claymoreno1@users.noreply.github.com>
2019-06-09 22:57:27 +0800

    Initial commit

---
+++ Assets/Script/Post.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Post : MonoBehaviour {
+    public Material mat;
+    private Camera cam;
+    private Vector2 pos;
+	void Start () {
+        cam = this.GetComponent<Camera>();
+	}
+    private void Update()
+    {
+        pos = new Vector2(Input.mousePosition.x / cam.pixelWidth, Input.mousePosition.y / cam.pixelHeight);
+    }
+    [ImageEffectOpaque]
+    private void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        mat.SetVector("pos1", pos);
+        Graphics.Blit(source, dest, mat);
+    }
+}
---
+++ Assets/Script/Post2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+//[ExecuteInEditMode]
+public class Post2 : MonoBehaviour {
+    public Material water;
+    public Camera cam;
+    private int width;
+    private int height;
+    private RenderTexture rt;
+    private Texture2D texture;
+    //private void Start()
+    //{
+    //    width = cam.pixelWidth;
+    //    height = cam.pixelHeight;
+    //    rt = RenderTexture.GetTemporary(width, height, 0);
+    //    cam.targetTexture = rt;
+
+    //}
+    [ImageEffectOpaque]
+    private void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+
+        Graphics.Blit(source, dest, water);
+
+    }
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+    //        RenderTexture.active = rt;
+    //        texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+    //        texture.Apply();
+    //        // RenderTexture.ReleaseTemporary(rt);
+    //        byte[] bytes = texture.EncodeToPNG();
+    //        File.WriteAllBytes("D:\\water.PNG", bytes);
+    //    }
+    //}
+}
